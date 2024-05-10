9f6ce3dee2144988b769dbd14ef73ba0985ebff6
Ayo <Ayo.meshe@gmail.com>
2024-05-10 16:07:12 +0100

    app update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,74 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import { ChevronDown } from "lucide-react";
+import { useState } from "react";
+
+// a const value that never changes - we put in caps!
+// 'as const' lets typescript know that the following is always an array that can not be modified
+
+const SORT_OPTIONS = [
+  { name: "None", value: "none" },
+  { name: "Price: Low to High", value: "price-asc" },
+  { name: "Price: High to Low", value: "price-desc" },
+] as const;
+
 export default function Home() {
+  const [filter, setFilter] = useState({
+    sort: "none",
+  });
+
+  console.log(filter);
+
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900">
           High-quality cotton selection
         </h1>
+        <div className="flex items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="group inline-flex justify-center text-sm font-mediium text-gray-700 hover:text-gray-900">
+              SORT
+              <ChevronDown className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="end">
+              {SORT_OPTIONS.map((option) => (
+                <button
+                  key={option.name}
+                  className={cn("text-left w-full block px-4 py-2 text-sm", {
+                    "text-gray-900 bg-gray-100": option.value === filter.sort,
+                    "text-gray-500": option.value !== filter.sort,
+                  })}
+                  onClick={() => {
+                    setFilter((prev) => ({
+                      ...prev,
+                      sort: option.value,
+                    }));
+                  }}
+                >
+                  {option.name}
+                </button>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
     </main>
   );
 }
+
+// Because we defined 'group' in our DropdownMenuTrigger, the same will apply to ChevronDown
+
+// 'Align' in DropdownMenuContent determines the position of where the dropdown menu will open
+
+// The cn function, short for "classnames," is a utility function used to conditionally concatenate CSS class names based on certain conditions.
